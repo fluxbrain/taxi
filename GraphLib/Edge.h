721ab4896b1c1d45723466721a5ed14829e593f8
Oke <fluxbrain@gmail.com>
2020-03-08 20:46:13 +0100

    add taxi project

---
+++ GraphLib/Edge.h
@@ -0,0 +1,34 @@
+#ifndef EDGE_H
+#define EDGE_H
+
+#include "Node.h"
+
+
+class Edge
+{
+public:
+
+    Edge(Node& rSrc, Node& rDst);
+
+    Edge(const Edge& rOther);
+
+    virtual ~Edge();
+
+    bool isConnectedTo(const Node& rNode) const;
+
+	Node& getSrcNode() { return m_rSrc; }
+	Node& getDstNode() { return m_rDst; }
+
+	std::string toString() const;
+
+	virtual double getWeight() { return 1; }
+
+
+private:
+
+	Node& m_rSrc;
+	Node& m_rDst;
+
+};
+
+#endif
